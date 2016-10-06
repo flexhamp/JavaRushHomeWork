fa80aa4abe0926085a2ad5a639bd9f1ed4141791
flexhamp <flexhamp@gmail.com>
2016-10-06 18:56:29 +0700

    1

--- src/com/javarush/test/level08/lesson08/task02/Solution.java
+++ src/com/javarush/test/level08/lesson08/task02/Solution.java
@@ -1,8 +1,8 @@
 package com.javarush.test.level08.lesson08.task02;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.Set;
 
 /* Удалить все числа больше 10
@@ -12,47 +12,32 @@ import java.util.Set;
 
 public class Solution
 {
+
+
     public static HashSet<Integer> createSet()
     {
+        Set<Integer> integerSet = new HashSet<>();
         //напишите тут ваш код
-        HashSet<Integer> hashSet = new HashSet<>();
         for (int i = 0; i < 20; i++)
         {
-            hashSet.add(i);
+            integerSet.add(i);
         }
-        return hashSet;
-
+        return (HashSet<Integer>) integerSet;
     }
 
     public static HashSet<Integer> removeAllNumbersMoreThan10(HashSet<Integer> set)
     {
+        LinkedList<Integer> linkedList = new LinkedList<>();
         //напишите тут ваш код
-        Iterator<Integer> iterator = set.iterator();
         int i = 0;
-        Integer number;
-        while (iterator.hasNext()){
-            number = iterator.next();
-            if (number > 10){
-                set.remove(0);
+        Iterator iterator = set.iterator();
+
+        while (iterator.hasNext()) {
+            if ((Integer) iterator.next() >= 10)
+            {
+                iterator.remove();
             }
-            i++;
         }
         return set;
-    }
-
-    public static void main(String[] args)
-    {
-        HashSet<Integer> hashSet = createSet();
-        Iterator iterator = hashSet.iterator();
-        while (iterator.hasNext()){
-            System.out.println(iterator.next());
-        }
-
-        System.out.println("Измененный список: ");
-        hashSet = removeAllNumbersMoreThan10(hashSet);
-        Iterator iterator1 = hashSet.iterator();
-        while (iterator1.hasNext()){
-            System.out.println(iterator1.next());
-        }
     }
 }
